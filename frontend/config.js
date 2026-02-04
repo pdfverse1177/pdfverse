5ad53baf8d190857ed4598d59656decc472a194f
pdfverse1177 <shehnazali1177@gmail.com>
2026-02-04 18:16:23 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -3,4 +3,4 @@ window.PDFVERSE_CONFIG = {
     API_URL: window.location.hostname === 'localhost' 
         ? 'http://localhost:5000/api'
         : 'https://pdfverse-production.up.railway.app/api'
-};    
+};
